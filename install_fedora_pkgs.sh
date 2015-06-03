07c1daabd234f8a10b24185c410b0887ed50fd12
Jonathon Reinhart <jreinhart@cc-sw.com>
2015-06-03 11:24:22 -0400

    add install_fedora_pkgs.sh

---
+++ install_fedora_pkgs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo yum install \
+    glib2.i686 \
+    freetype.i686 \
+    libSM.i686 \
+    libICE.i686 \
+    libXrender.i686 \
+    libX11.i686 \
+    libXext.i686 \
+    fontconfig.i686 \
+    python-libs.i686
