3a7c0142cb80e389fc5be6e94a63cb9ca8e41993
Jonathon Reinhart <jreinhart@cc-sw.com>
2015-06-03 11:35:46 -0400

    Add ida64.desktop and copy in install_fedora_pkgs.sh
    This is hard-coded to ida-6.8 right now, but changing
    install directories to just /opt/ida/ would fix this...

--- install_fedora_pkgs.sh
+++ install_fedora_pkgs.sh
@@ -2,6 +2,9 @@
 # IDA Linux is a 32-bit application (even IDA 64).
 # Run this script when installing IDA Linux to ensure
 # all required 32-bit libraries are installed.
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
 sudo yum install \
     glib2.i686 \
     freetype.i686 \
@@ -12,3 +15,6 @@ sudo yum install \
     libXext.i686 \
     fontconfig.i686 \
     python-libs.i686
+
+# Install desktop shortcut (launcher)
+cp $DIR/ida64.desktop ~/.local/share/applications/
